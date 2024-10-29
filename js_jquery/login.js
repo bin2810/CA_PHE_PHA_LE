b0cb89c3d7d7c388d576950cb1318c05f8b9acd1
bin2810 <bin200528@gmail.com>
2024-10-29 15:50:08 +0700

    29/10/2024 3:50DM

--- js_jquery/login.js
+++ js_jquery/login.js
@@ -1,24 +1,23 @@
-$(function(){
+$(function() {
     $("#lop_phu_login").hide(); // ẩn form 
 
-    
+
     $("#btnlogin").click(function() {
         $("#lop_phu_login").fadeIn(); // Hiển thị form 
-        $('body').addClass('lock-scroll');  // Thêm class để khóa cuộn
+        $('body').addClass('lock-scroll'); // Thêm class để khóa cuộn
     });
 
     // Khi người dùng bấm vào lớp phủ bên ngoài để đóng
     $("#lop_phu_login, #btnlogin").click(function(event) {
-        if (event.target.id ) { //id của #lop_phu_login
+        if (event.target.id === "lop_phu_sp") { //id của #lop_phu_login
             $(this).fadeOut(); // Ẩn lớp phủ và form đăng nhập
-            $('body').removeClass('lock-scroll');  // Thêm class để khóa cuộn
+            $('body').removeClass('lock-scroll'); // Thêm class để khóa cuộn
         }
     });
-    
-    $(".nut_login").click(function(){
+
+    $(".nut_login").click(function() {
         $("#lop_phu_login").fadeOut(); // ẩn form   
-        $('body').removeClass('lock-scroll');  
+        $('body').removeClass('lock-scroll');
     })
 
 });
-

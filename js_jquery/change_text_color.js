7362dde4371af602094e01ce47d3a024da1b5f20
bin2810 <bin200528@gmail.com>
2024-11-15 09:51:13 +0700

    15/11/2024

--- js_jquery/change_text_color.js
+++ js_jquery/change_text_color.js
@@ -1,30 +1,18 @@
 $(function() {
-    $(window).resize(function() {
-        if (window.matchMedia("(min-width: 1200px)").matches) {
-            // Chạy mã JavaScript khi màn hình rộng hơn 768px
-            // Ví dụ: Khởi tạo một slider, hiệu ứng hover phức tạp, v.v.
-            $(".product_col_1, .product_col_2, .product_col_3").hover(function() {
-                $(".product_information_content", this).removeClass("demo1").addClass("demo2");
-                $(".product_information_content", this).addClass("move-left"); // Thêm lớp di chuyển
-            }, function() {
-                $(".product_information_content", this).addClass("demo1").removeClass("demo2");
-                $(".product_information_content", this).removeClass("move-left"); // Bỏ lớp di chuyển
-            });
-        
-            $(".spnb").hover(function() {
-                $(".namespnb", this).removeClass("demo3").addClass("demo4");
-                $(".namespnb", this).addClass("move-left1");
-            }, function() {
-                $(".namespnb", this).addClass("demo3").removeClass("demo4");
-                $(".namespnb", this).removeClass("move-left1");
-            });
-            
-        } else {
-            $(".product_col_1, .product_col_2, .product_col_3, .spnb")
-        .off('hover mouseover mouseout')
-        .addClass("no-hover"); // Thêm lớp CSS để áp dụng các kiểu cho màn hình nhỏ
-        }
+    $(".product_col_1, .product_col_2, .product_col_3").hover(function() {
+        $(".product_information_content", this).removeClass("demo1").addClass("demo2");
+            $(".product_information_content", this).addClass("move-left"); // Thêm lớp di chuyển
+    }, function() {
+        $(".product_information_content", this).addClass("demo1").removeClass("demo2");
+        $(".product_information_content", this).removeClass("move-left"); // Bỏ lớp di chuyển
     });
-    $(window).trigger('resize');
-   
+        
+    $(".spnb").hover(function() {
+        $(".namespnb", this).removeClass("demo3").addClass("demo4");
+        $(".namespnb", this).addClass("move-left1");
+    }, function() {
+        $(".namespnb", this).addClass("demo3").removeClass("demo4");
+        $(".namespnb", this).removeClass("move-left1");
+    });
+    
 });

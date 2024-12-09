0fff3ca2dcca372ccd0d0c7540c9b72b83436a83
bin2810 <bin200528@gmail.com>
2024-12-09 21:00:25 +0700

    oj

---
+++ js_jquery/error_phone.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.nut_login').click(function () {
+        var phoneNumber = $('.nhap_phone').val();
+        if (phoneNumber === '') {
+            $('.error').text('Số điện thoại không được để trống').show();
+            return false; 
+        }        
+        if (phoneNumber.length < 10 || phoneNumber.length > 11) {
+            $('.error').text('Số điện thoại không hợp lệ. Vui lòng nhập lại!').show();
+            return false;
+        }
+        $('.error').hide();
+        
+        $("#lop_phu_login").hide();
+
+    });
+});
--- js_jquery/login.js
+++ js_jquery/login.js
@@ -1,19 +1,17 @@
 $(function() {
-    // Tải nội dung của login.html
+    
     $.get('/html/login.html', function(data) {
         $('body').append(data);
 
-        // Ẩn form mặc định (nếu cần)
+      
         $("#lop_phu_login").hide();
 
-        // Sự kiện click vào nút đăng nhập
         $("#btnlogin , #btnlogin_tablet").click(function() {
-            $("#lop_phu_login").fadeIn();; // Hiển thị form mượt mà hơn
+            $("#lop_phu_login").fadeIn();; 
             $('body').addClass('lock-scroll');
         });
         
 
-        // Sự kiện click vào vùng bên ngoài form để đóng
         $(document).click(function(event) {
             if (event.target.id === "lop_phu_login"){
                 $("#lop_phu_login").fadeOut();

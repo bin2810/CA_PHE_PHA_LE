cc1085b72a44511f25cd097814b79ac08a1bc01a
bin2810 <bin200528@gmail.com>
2024-12-13 20:04:52 +0700

    upfull13/12

--- js_jquery/change_address.js
+++ js_jquery/change_address.js
@@ -9,11 +9,26 @@ $(function(){
 
     // Khi người dùng bấm vào lớp phủ bên ngoài để đóng
     $("#lop_phu_address").click(function(event) {
-        if (event.target.id ) { //id của #lop_phu_login
+        if (event.target.id === "lop_phu_address" ) { //id của #lop_phu_login
             $(this).fadeOut(); // Ẩn lớp phủ và form đăng nhập
             $('body').removeClass('lock-scroll');  // Thêm class để khóa cuộn
         }
     });
+
+    $("#btn_address_close").click(function() {
+        $("#lop_phu_address").fadeOut(); // Hiển thị form 
+        $('body').removeClass('lock-scroll');  // Thêm class để khóa cuộn
+        var inputvalue = $('#txt_address').val()
+        $('#hide_address').text(inputvalue);
+    });
+
+
+    $("#btn_new_address").click(function() {
+        $('#txt_address').val('') 
+    });
+    
+
+    
     
 })
 
--- js_jquery/change_text_color.js
+++ js_jquery/change_text_color.js
@@ -1,18 +1,26 @@
 $(function() {
     $(".product_col_1, .product_col_2, .product_col_3").hover(function() {
         $(".product_information_content", this).removeClass("demo1").addClass("demo2");
-            $(".product_information_content", this).addClass("move-left"); // Thêm lớp di chuyển
+        $(".product_information_content", this).addClass("move-left"); // Thêm lớp di chuyển
+        $(".product_information_money", this).removeClass("demo3").addClass("demo4");
+        $(".product_information_money", this).addClass("move-left2");
     }, function() {
         $(".product_information_content", this).addClass("demo1").removeClass("demo2");
         $(".product_information_content", this).removeClass("move-left"); // Bỏ lớp di chuyển
+        $(".product_information_money", this).addClass("demo3").removeClass("demo4");
+        $(".product_information_money", this).removeClass("move-left2");
     });
         
     $(".spnb").hover(function() {
         $(".namespnb", this).removeClass("demo3").addClass("demo4");
         $(".namespnb", this).addClass("move-left1");
+        $(".price", this).removeClass("demo3").addClass("demo4");
+        $(".price", this).addClass("move-left2");
     }, function() {
         $(".namespnb", this).addClass("demo3").removeClass("demo4");
         $(".namespnb", this).removeClass("move-left1");
+        $(".price", this).addClass("demo3").removeClass("demo4");
+        $(".price", this).removeClass("move-left2");
     });
     
 });

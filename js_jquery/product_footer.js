4c90e16c1c9ebb17c9c5c9009523a38303f9bf6e
bin2810 <bin200528@gmail.com>
2024-12-17 19:56:53 +0700

    full17/12

--- js_jquery/product_footer.js
+++ js_jquery/product_footer.js
@@ -1,16 +1,13 @@
 $(function() {
     $(window).scroll(function() {
-        // Khoảng cách từ đầu trang đến cuối màn hình + chiều cao màn hình
         var scrollPosition = $(window).scrollTop() + $(window).height();
 
-        // Vị trí từ đầu trang đến id cuối
         var footerPosition = $('#cuoi').offset().top;
 
-        // Nếu vị trí cuộn gần đến footer
-        if (scrollPosition >= footerPosition - 5) { // Điều chỉnh khoảng cách ở đây
-            $('.product_menu').fadeOut(1); // Ẩn đối tượng
+        if (scrollPosition >= footerPosition - 5) { 
+            $('.product_menu').fadeOut(1); 
         } else {
-            $('.product_menu').fadeIn(); // Hiện lại đối tượng khi cuộn lên
+            $('.product_menu').fadeIn(); 
         }
     });
 });

c62d56837ded7ec8c7fa852f5c58926c48b9f393
bin2810 <bin200528@gmail.com>
2024-12-20 17:15:38 +0700

    20/12 full

--- js_jquery/check_lienhe.js
+++ js_jquery/check_lienhe.js
@@ -5,16 +5,18 @@ $(function(){
         var email_mau_lienhe = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
         var email_lienhe = $('#email_lienhe').val();
         var noidung_lienhe = $('#noidung_lienhe').val();
+        var phoneNumber = $('#nhap_phone').val();
         var error_name_lienhe = $('.error_name_lienhe');
         var error_email_lienhe = $('.error_email_lienhe');
         var error_noidung_lienhe = $('.error_noidung_lienhe');
+        var error_sdt_lienhe = $('.error_sdt_lienhe');
 
         error_name_lienhe.hide()
         error_email_lienhe.hide()
         error_noidung_lienhe.hide()
 
         if (name_lienhe === '') {
-            error_name_lienhe.text('Họ và tê không được bỏ trống').show();
+            error_name_lienhe.text('Họ và tên không được bỏ trống').show();
             setTimeout(function() {
                 error_name_lienhe.hide();
             }, 4000);
@@ -34,6 +36,16 @@ $(function(){
             setTimeout(function() {
                 error_noidung_lienhe.hide();
             }, 4000);
+            return false;
+        }
+
+        
+        if (phoneNumber === '') {
+            error_sdt_lienhe.text('Số điện thoại không được để trống').show();
+            return false; 
+        }        
+        if (phoneNumber.length < 10 || phoneNumber.length > 11) {
+            error_sdt_lienhe.text('Số điện thoại không hợp lệ. Vui lòng nhập lại!').show();
             return false;
         }
     });
